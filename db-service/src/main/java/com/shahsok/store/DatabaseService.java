d85eae07c4f1226720cd499eac4abfbce8db767d
Ruslan <ruslanshashok322@gmail.com>
2023-12-16 23:35:55 +0200

    init

---
+++ db-service/src/main/java/com/shahsok/store/DatabaseService.java
@@ -0,0 +1,13 @@
+package com.shahsok.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseService.class, args);
+    }
+
+}
